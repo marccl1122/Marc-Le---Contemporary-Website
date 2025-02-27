17ec859eec652b3f66099859ceccb71a069da440
Marc <marcoibmcoop@gmail.com>
2025-02-26 20:06:57 -0800

    typeerror 5

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'; // Assuming you're using framer-motion f
 import { siteConfig } from '@/lib/constants';
 
 interface GalleryProps {
-  className: string; // Optional className prop
+  className: any; 
 }
 
 const Gallery: React.FC<GalleryProps> = ({ className }) => {
